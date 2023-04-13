b60e3824324e62c16d0ec3ec824980cad5ced6f4
Ethan Beneroff <ethanbeneroff@gmail.com>
2023-04-13 13:07:05 -0400

    completes lab

--- src/index.js
+++ src/index.js
@@ -1,5 +1,24 @@
 const init = () => {
-  
+    const inputForm = document.querySelector("form");
+    inputForm.addEventListener("submit", (event) => {
+        event.preventDefault()
+        //console.log(event.target.children[1].value)
+        //let searchValue = document.getElementById("searchById").value   //why doesnt this work?
+        const searchValue = document.querySelector("input#searchByID").value;
+        console.log(searchValue)
+
+
+        fetch(`http://localhost:3000/movies/${searchValue}`)
+        .then((response) => response.json())
+        .then((data) => {
+            const title = document.querySelector("section#movieDetails h4")
+            const summary = document.querySelector("section#movieDetails p")
+            title.innerText=data.title
+            summary.innerText = data.summary
+            //console.log(data)
+        })
+    })
+
 }
 
 document.addEventListener('DOMContentLoaded', init);
